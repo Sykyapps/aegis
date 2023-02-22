2095aa7a0a0dfc94d84ea179299aa5bf0767b3f9
Dany Rizkiawan <dany@sayakaya.id>
2023-02-22 12:02:21 +0700

    Add colors
    a

--- lib/foundation.dart
+++ lib/foundation.dart
@@ -1,3 +1,4 @@
 library foundation;
 
 export 'src/foundation/typography.dart';
+export 'src/foundation/colors.dart';
---
+++ lib/src/foundation/colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AegisColors {
+  static const neutral0 = Color.fromRGBO(255, 255, 255, 1);
+  static const neutral100 = Color.fromRGBO(245, 246, 250, 1);
+  static const neutral200 = Color.fromRGBO(224, 227, 239, 1);
+  static const neutral300 = Color.fromRGBO(152, 156, 172, 1);
+  static const neutral400 = Color.fromRGBO(86, 91, 107, 1);
+  static const neutral500 = Color.fromRGBO(28, 29, 31, 1);
+  static const neutral600 = Color.fromRGBO(0, 0, 0, 1);
+
+  static const purple100 = Color.fromRGBO(236, 234, 247, 1);
+  static const purple200 = Color.fromRGBO(160, 149, 216, 1);
+  static const purple300 = Color.fromRGBO(103, 86, 193, 1);
+  static const purple400 = Color.fromRGBO(83, 71, 148, 1);
+
+  static const blue100 = Color.fromRGBO(238, 240, 252, 1);
+  static const blue200 = Color.fromRGBO(136, 153, 231, 1);
+  static const blue300 = Color.fromRGBO(77, 104, 220, 1);
+  static const blue400 = Color.fromRGBO(58, 80, 171, 1);
+
+  static const tosca100 = Color.fromRGBO(230, 254, 252, 1);
+  static const tosca200 = Color.fromRGBO(112, 218, 211, 1);
+  static const tosca300 = Color.fromRGBO(20, 198, 198, 1);
+  static const tosca400 = Color.fromRGBO(11, 161, 161, 1);
+
+  static const green100 = Color.fromRGBO(238, 255, 249, 1);
+  static const green200 = Color.fromRGBO(112, 218, 181, 1);
+  static const green300 = Color.fromRGBO(20, 198, 135, 1);
+  static const green400 = Color.fromRGBO(19, 175, 120, 1);
+
+  static const yellow100 = Color.fromRGBO(253, 255, 229, 1);
+  static const yellow200 = Color.fromRGBO(255, 238, 148, 1);
+  static const yellow300 = Color.fromRGBO(255, 222, 49, 1);
+
+  static const orange100 = Color.fromRGBO(255, 235, 229, 1);
+  static const orange200 = Color.fromRGBO(255, 157, 126, 1);
+  static const orange300 = Color.fromRGBO(255, 98, 49, 1);
+
+  static const red100 = Color.fromRGBO(255, 229, 236, 1);
+  static const red200 = Color.fromRGBO(255, 126, 159, 1);
+  static const red300 = Color.fromRGBO(255, 49, 101, 1);
+}
