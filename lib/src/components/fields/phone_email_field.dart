1350fbcdb98bf4819926d02f6a69d46cdc8dab0d
Dany Rizkiawan <dany@sayakaya.id>
2023-03-15 16:57:18 +0700

    Add suffix to phone email field

--- lib/src/components/fields/phone_email_field.dart
+++ lib/src/components/fields/phone_email_field.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:form_validator/form_validator.dart';
 
-import '../../../foundation.dart';
 import 'fields.dart';
+import 'phone/phone_prefix.dart';
+import 'phone/phone_suffix.dart';
 
 class SkPhoneEmailField extends HookWidget {
   const SkPhoneEmailField({
@@ -37,46 +36,11 @@ class SkPhoneEmailField extends HookWidget {
       hintText: 'Masukkan nomor ponsel atau email.',
       errorText: errorMessage,
       controller: controller,
-      prefix: isPhoneMin && !hasNonDigit ? const _PhonePrefix() : null,
+      prefix: isPhoneMin && !hasNonDigit ? const PhonePrefix() : null,
+      suffix: isPhoneMin && !hasNonDigit ? const PhoneSuffix() : null,
       validator: ValidationBuilder(requiredMessage: requiredMessage)
           .add(RegExpValidator.phoneEmailValidator)
           .build(),
-    );
-  }
-}
-
-class _PhonePrefix extends StatelessWidget {
-  const _PhonePrefix({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AegisColors.neutral100,
-        borderRadius: BorderRadius.circular(2).r,
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 4).r,
-      margin: const EdgeInsets.only(right: 8).r,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SvgPicture.network(
-            'https://storage.googleapis.com/sayakaya-static/country_flags/indonesia.svg',
-            width: 25.r,
-          ),
-          SizedBox(width: 4.r),
-          Text(
-            '+62',
-            style: AegisFont.bodyLarge.copyWith(
-              color: AegisColors.neutral400,
-              height: 24 / 16,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          SizedBox(width: 4.r),
-          Icon(Icons.keyboard_arrow_down, size: 16.r),
-        ],
-      ),
     );
   }
 }
