6078d93ecc99a20f2134f824e68f06e6ccd1d54a
Yosyafat Aulia <yosyafat@sayakaya.id>
2023-03-21 14:46:58 +0700

    Update icon list

--- lib/src/icons/icon_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class AegisIconData extends IconData {
-  const AegisIconData(super.codePoint)
-      : super(
-          fontFamily: 'AegisIcons',
-          fontPackage: 'aegis',
-        );
-}
--- lib/src/icons/icons.dart
+++ lib/src/icons/icons.dart
@@ -1,14 +1,103 @@
+// Place fonts/AegisIcons.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: AegisIcons
+//      fonts:
+//       - asset: fonts/AegisIcons.ttf
 // ignore_for_file: constant_identifier_names
 
-import 'icon_data.dart';
+import 'package:flutter/widgets.dart';
 
 class AegisIcons {
-  static const chevron_left = AegisIconData(0xe900);
-  static const circle_check = AegisIconData(0xe901);
-  static const circle_close = AegisIconData(0xe902);
-  static const close = AegisIconData(0xe903);
-  static const contacts_book = AegisIconData(0xe904);
-  static const eye_off = AegisIconData(0xe905);
-  static const eye = AegisIconData(0xe906);
-  static const shield_lock = AegisIconData(0xe907);
+  AegisIcons._();
+
+  static const String _fontFamily = 'AegisIcons';
+
+  static const IconData add_image = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData add_line = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData alert_fill = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData alert_line = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData arrow_down_line = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData arrow_in_line = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData arrow_left_line = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData arrow_out_line = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData arrow_right_line = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData arrow_up_line = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData bookmark_fill = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData bookmark_line = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData camera_fill = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData camera_line = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData chat = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData check_line = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData chevron_down_line = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData chevron_left_line = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData chevron_right_line = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData chevron_up_line = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData circlecheck_fill = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData circlecheck_line = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData close_circle_fill = IconData(0xe916, fontFamily: _fontFamily);
+  static const IconData close_circle_line = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData close_line_line = IconData(0xe918, fontFamily: _fontFamily);
+  static const IconData compare = IconData(0xe919, fontFamily: _fontFamily);
+  static const IconData contact = IconData(0xe91a, fontFamily: _fontFamily);
+  static const IconData copy = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData delete = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData download_fill = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData download_line = IconData(0xe91e, fontFamily: _fontFamily);
+  static const IconData edit_fill = IconData(0xe91f, fontFamily: _fontFamily);
+  static const IconData edit_line = IconData(0xe920, fontFamily: _fontFamily);
+  static const IconData equal_line = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData eye_close_line = IconData(0xe922, fontFamily: _fontFamily);
+  static const IconData eye_open_line = IconData(0xe923, fontFamily: _fontFamily);
+  static const IconData face_id = IconData(0xe924, fontFamily: _fontFamily);
+  static const IconData file_fill = IconData(0xe925, fontFamily: _fontFamily);
+  static const IconData file_line = IconData(0xe926, fontFamily: _fontFamily);
+  static const IconData filter = IconData(0xe927, fontFamily: _fontFamily);
+  static const IconData fingerprint = IconData(0xe928, fontFamily: _fontFamily);
+  static const IconData flash_fill = IconData(0xe929, fontFamily: _fontFamily);
+  static const IconData graph = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData heart_fill = IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData heart_line = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData home_fill = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData home_line = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData information_fill = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData information_line = IconData(0xe931, fontFamily: _fontFamily);
+  static const IconData lock_fill = IconData(0xe932, fontFamily: _fontFamily);
+  static const IconData menu = IconData(0xe933, fontFamily: _fontFamily);
+  static const IconData money = IconData(0xe934, fontFamily: _fontFamily);
+  static const IconData morehorizontal = IconData(0xe935, fontFamily: _fontFamily);
+  static const IconData morevertical = IconData(0xe936, fontFamily: _fontFamily);
+  static const IconData netral = IconData(0xe937, fontFamily: _fontFamily);
+  static const IconData order_history = IconData(0xe938, fontFamily: _fontFamily);
+  static const IconData placeholder_line = IconData(0xe939, fontFamily: _fontFamily);
+  static const IconData play = IconData(0xe93a, fontFamily: _fontFamily);
+  static const IconData portfolio = IconData(0xe93b, fontFamily: _fontFamily);
+  static const IconData product = IconData(0xe93c, fontFamily: _fontFamily);
+  static const IconData profil = IconData(0xe93d, fontFamily: _fontFamily);
+  static const IconData promocode = IconData(0xe93e, fontFamily: _fontFamily);
+  static const IconData question = IconData(0xe93f, fontFamily: _fontFamily);
+  static const IconData refresh_line = IconData(0xe940, fontFamily: _fontFamily);
+  static const IconData restart_line = IconData(0xe941, fontFamily: _fontFamily);
+  static const IconData safety = IconData(0xe942, fontFamily: _fontFamily);
+  static const IconData scale = IconData(0xe943, fontFamily: _fontFamily);
+  static const IconData scan_line = IconData(0xe944, fontFamily: _fontFamily);
+  static const IconData search_fill = IconData(0xe945, fontFamily: _fontFamily);
+  static const IconData search_line = IconData(0xe946, fontFamily: _fontFamily);
+  static const IconData send_plane_fill = IconData(0xe947, fontFamily: _fontFamily);
+  static const IconData send_plane_line = IconData(0xe948, fontFamily: _fontFamily);
+  static const IconData share = IconData(0xe949, fontFamily: _fontFamily);
+  static const IconData shield_check_fill = IconData(0xe94a, fontFamily: _fontFamily);
+  static const IconData shield_keyhole_fill = IconData(0xe94b, fontFamily: _fontFamily);
+  static const IconData sort = IconData(0xe94c, fontFamily: _fontFamily);
+  static const IconData sortandfilter = IconData(0xe94d, fontFamily: _fontFamily);
+  static const IconData star_fill = IconData(0xe94e, fontFamily: _fontFamily);
+  static const IconData star_line = IconData(0xe94f, fontFamily: _fontFamily);
+  static const IconData target = IconData(0xe950, fontFamily: _fontFamily);
+  static const IconData time = IconData(0xe951, fontFamily: _fontFamily);
+  static const IconData transactions = IconData(0xe952, fontFamily: _fontFamily);
+  static const IconData user_fill = IconData(0xe953, fontFamily: _fontFamily);
+  static const IconData user_line = IconData(0xe954, fontFamily: _fontFamily);
+  static const IconData verified = IconData(0xe955, fontFamily: _fontFamily);
 }
