082122fda69d315f519fed0561ef9ad2941a3d09
Ahmad Dhiyaaul Auliyaa <freelance@sayakaya.id>
2023-03-26 13:14:31 +0700

    feature(segmented_ctrl): add height param to item

--- lib/src/components/buttons/segmented_control/segmented_control_item.dart
+++ lib/src/components/buttons/segmented_control/segmented_control_item.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 
 class SkSegmentedControlItem extends StatelessWidget {
   final String? label;
+  final double? height;
 
   const SkSegmentedControlItem({
     Key? key,
     this.label,
+    this.height,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      height: height,
       width: double.maxFinite,
+      alignment: Alignment.center,
       child: Text(
         '$label',
         textAlign: TextAlign.center,
