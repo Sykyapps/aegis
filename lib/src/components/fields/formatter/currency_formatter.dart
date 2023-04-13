49cd2b8bc1243536d820eef2c07ed4ab1160673a
Yosyafat Aulia <yosyafat@sayakaya.id>
2023-04-14 00:43:24 +0700

    add: change currency input formatter

--- lib/src/components/fields/formatter/currency_formatter.dart
+++ lib/src/components/fields/formatter/currency_formatter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import '../../../utils/currency_util.dart';
 
@@ -33,19 +34,65 @@ class CurrencyInputFormatter extends TextInputFormatter {
 
   @override
   TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
     if (newValue.selection.baseOffset == 0) {
       return newValue;
     }
 
-    var cleanDigit = filterDigit(newValue.text);
-    if (cleanDigit.isEmpty) return TextEditingValue.empty;
-    double value = double.parse(cleanDigit);
-    String newText = format(value.toString());
+    double value = double.parse(newValue.text);
+    final fmt = NumberFormat.simpleCurrency(
+      locale: 'id_ID',
+      decimalDigits: 0,
+    );
 
-    int offset =
-        newValue.selection.baseOffset <= 4 ? 4 : newValue.selection.baseOffset;
+    final formattedValue = fmt.format(value);
     return newValue.copyWith(
-        text: newText, selection: TextSelection.collapsed(offset: offset));
+      text: formattedValue,
+      selection: TextSelection.collapsed(
+        offset: formattedValue.length,
+      ),
+    );
   }
 }
+
+// TextEditingValue formatEditUpdate(
+//     TextEditingValue oldValue, TextEditingValue newValue) {
+//   if (newValue.selection.baseOffset == 0) {
+//     return newValue;
+//   }
+
+//   var cleanDigit = filterDigit(newValue.text);
+//   if (cleanDigit.isEmpty) return TextEditingValue.empty;
+//   double value = double.parse(cleanDigit);
+//   String newText = format(value.toString());
+
+//   int offset =
+//       newValue.selection.baseOffset <= 4 ? 4 : newValue.selection.baseOffset;
+//   return newValue.copyWith(
+//       text: newText, selection: TextSelection.collapsed(offset: offset));
+// }
+
+// @yos-syky references
+// TextInputFormatter.withFunction(
+//   (oldValue, newValue) {
+//     if (newValue.selection.baseOffset == 0) {
+//       return newValue;
+//     }
+
+//     double value = double.parse(newValue.text);
+//     final fmt = NumberFormat.simpleCurrency(
+//       locale: 'id_ID',
+//       decimalDigits: 0,
+//     );
+
+//     final formattedValue = fmt.format(value);
+//     return newValue.copyWith(
+//       text: formattedValue,
+//       selection: TextSelection.collapsed(
+//         offset: formattedValue.length,
+//       ),
+//     );
+//   },
+// )
