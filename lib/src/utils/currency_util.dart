ab3076687317efc686be0392b00a9adcd14a5d3c
Yosyafat Aulia <yosyafat@sayakaya.id>
2023-04-17 15:56:16 +0700

    add: decimalFormat, compactFormat to CurrencyUtil

--- lib/src/utils/currency_util.dart
+++ lib/src/utils/currency_util.dart
@@ -3,13 +3,33 @@ import 'package:intl/intl.dart';
 class CurrencyUtil {
   static var formatter = NumberFormat.currency(
     locale: 'id',
-    symbol: 'Rp ',
+    symbol: 'Rp',
     decimalDigits: 0,
     customPattern: '\u00a4#,###',
   );
 
   static String format(dynamic value) {
     return formatter.format(value);
+  }
+
+   static String compactFormat(dynamic value) {
+    var formatter = NumberFormat.compactCurrency(
+      locale: 'id',
+      symbol: '',
+      decimalDigits: 2,
+    );
+    return formatter.format(value).replaceAll(RegExp(r'\s+'), '');
+  }
+
+  static String decimalFormat(dynamic value, {int? decimalDigits = 0}) {
+    var decimal = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp',
+      decimalDigits: decimalDigits,
+      customPattern: '\u00a4#,###',
+    );
+
+    return decimal.format(value);
   }
 
   static double parse(String value) {
