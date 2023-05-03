b53a0857738e081c8447bd5830e873e8233262e3
Dany Rizkiawan <dany@sayakaya.id>
2023-05-03 12:57:25 +0700

    add: group option to searchable bottom sheet

--- lib/src/components/bottom_sheet/searchable_options_bottom_sheet.dart
+++ lib/src/components/bottom_sheet/searchable_options_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:azlistview/azlistview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,8 +15,10 @@ class SkSearchableOptionsBottomSheet<T> extends HookWidget {
     required this.emptyTitle,
     required this.emptyDescription,
     required this.options,
-    required this.getImage,
     required this.getLabel,
+    this.getImage,
+    this.groupByAlphabet = false,
+    this.getTag,
   });
 
   final String title;
@@ -23,8 +26,10 @@ class SkSearchableOptionsBottomSheet<T> extends HookWidget {
   final String emptyTitle;
   final String emptyDescription;
   final List<T> options;
-  final String? Function(T) getImage;
   final String Function(T) getLabel;
+  final String? Function(T)? getImage;
+  final bool groupByAlphabet;
+  final String? Function(T)? getTag;
 
   static const radius = Radius.circular(16);
 
@@ -43,6 +48,18 @@ class SkSearchableOptionsBottomSheet<T> extends HookWidget {
   Widget build(BuildContext context) {
     var filtered = useState<List<T>>(options);
     var controller = useTextEditingController();
+
+    List<_AzData> grouped() {
+      var sorted = filtered.value
+          .map((f) => _AzData<T>(
+                data: f,
+                tag: getTag?.call(f) ?? getLabel(f)[0].toUpperCase(),
+              ))
+          .toList();
+      SuspensionUtil.sortListBySuspensionTag(sorted);
+      SuspensionUtil.setShowSuspensionStatus(sorted);
+      return sorted;
+    }
 
     void onChanged(String value) {
       value = value.toLowerCase();
@@ -83,14 +100,56 @@ class SkSearchableOptionsBottomSheet<T> extends HookWidget {
                       description: emptyDescription,
                     ),
                   )
+                else if (groupByAlphabet)
+                  SliverFillRemaining(
+                    child: AzListView(
+                      data: grouped(),
+                      itemCount: grouped().length,
+                      itemBuilder: (context, index) {
+                        var f = grouped()[index];
+                        return Column(
+                          children: [
+                            Offstage(
+                              offstage: !f.isShowSuspension,
+                              child: _AzGroup(tag: f.getSuspensionTag()),
+                            ),
+                            _OptionItem(
+                              title: getLabel(f.data),
+                              imageUrl: getImage?.call(f.data),
+                              onPressed: () =>
+                                  Navigator.of(context).pop(f.data),
+                            ),
+                          ],
+                        );
+                      },
+                      indexBarOptions: IndexBarOptions(
+                        needRebuild: true,
+                        textStyle: AegisFont.small.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: AegisColors.textHighEmphasis,
+                        ),
+                        indexHintAlignment: Alignment.centerRight,
+                        indexHintDecoration: BoxDecoration(
+                          color: AegisColors.blue300,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        indexHintWidth: 20,
+                        indexHintHeight: 20,
+                        indexHintTextStyle: AegisFont.small.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: AegisColors.neutral0,
+                        ),
+                      ),
+                    ),
+                  )
                 else
                   SliverList(
                     delegate: SliverChildListDelegate(
                       filtered.value
-                          .map((f) => ListTile(
-                                leading: _Image(imageUrl: getImage(f)),
-                                title: Text(getLabel(f)),
-                                onTap: () => Navigator.of(context).pop(f),
+                          .map((f) => _OptionItem(
+                                title: getLabel(f),
+                                imageUrl: getImage?.call(f),
+                                onPressed: () => Navigator.of(context).pop(f),
                               ))
                           .toList(),
                     ),
@@ -243,10 +302,47 @@ class _Empty extends StatelessWidget {
   }
 }
 
-class _Image extends StatelessWidget {
-  const _Image({Key? key, this.imageUrl}) : super(key: key);
+class _OptionItem extends StatelessWidget {
+  const _OptionItem({
+    Key? key,
+    required this.title,
+    this.imageUrl,
+    required this.onPressed,
+  }) : super(key: key);
 
+  final String title;
   final String? imageUrl;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20).r,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: AegisColors.borderHighEmphasis),
+        ),
+      ),
+      child: ListTile(
+        dense: true,
+        leading: imageUrl == null ? null : _Image(imageUrl: imageUrl!),
+        title: Text(
+          title,
+          style: AegisFont.bodyMedium.copyWith(
+            color: AegisColors.textHighEmphasis,
+          ),
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 8).r,
+        onTap: onPressed,
+      ),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image({Key? key, required this.imageUrl}) : super(key: key);
+
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -257,9 +353,40 @@ class _Image extends StatelessWidget {
         border: Border.all(color: AegisColors.borderHighEmphasis),
         borderRadius: BorderRadius.circular(4).r,
       ),
-      child: (imageUrl?.isEmpty ?? true)
+      child: imageUrl.isEmpty
           ? const SizedBox()
-          : Image.network(imageUrl!, width: 25.r),
+          : Image.network(imageUrl, width: 25.r),
+    );
+  }
+}
+
+class _AzData<T> extends ISuspensionBean {
+  final T data;
+  final String tag;
+
+  _AzData({required this.data, required this.tag});
+
+  @override
+  String getSuspensionTag() => tag;
+}
+
+class _AzGroup extends StatelessWidget {
+  const _AzGroup({Key? key, required this.tag}) : super(key: key);
+
+  final String tag;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8).r,
+      width: double.infinity,
+      color: AegisColors.backgroundGray,
+      child: Text(
+        tag,
+        style: AegisFont.bodyMedium.copyWith(
+          color: AegisColors.textHighEmphasis,
+        ),
+      ),
     );
   }
 }
