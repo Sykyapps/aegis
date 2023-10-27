73213aacd3a067f46fe8f0d680038fafbffd3fb8
Yosyafat Aulia <yosyafat@sayakaya.id>
2023-10-27 10:49:28 +0700

    rename svg file

--- lib/src/icons/icons.dart
+++ lib/src/icons/icons.dart
@@ -1,189 +1,201 @@
-// Place fonts/AegisIcons.ttf in your fonts/ directory and
-// add the following to your pubspec.yaml
-// flutter:
-//   fonts:
-//    - family: AegisIcons
-//      fonts:
-//       - asset: fonts/AegisIcons.ttf
-// ignore_for_file: constant_identifier_names
-
 import 'package:flutter/widgets.dart';
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: constant_identifier_names
+@immutable
+class _AegisIconsData extends IconData {
+  const _AegisIconsData(int codePoint, this.name)
+      : super(
+          codePoint,
+          fontFamily: 'AegisIcons',
+          fontPackage: 'aegis',
+        );
+
+  final String name;
+}
+
+@immutable
 class AegisIcons {
-  AegisIcons._();
+  const AegisIcons._();
 
-  static const String _fontFamily = 'AegisIcons';
+  static const add_image = _AegisIconsData(0xf101, 'add_image');
+  static const add = _AegisIconsData(0xf102, 'add');
+  static const alert_fill = _AegisIconsData(0xf103, 'alert_fill');
+  static const alert = _AegisIconsData(0xf104, 'alert');
+  static const arrow_down = _AegisIconsData(0xf105, 'arrow_down');
+  static const arrow_in = _AegisIconsData(0xf106, 'arrow_in');
+  static const arrow_left = _AegisIconsData(0xf107, 'arrow_left');
+  static const arrow_out = _AegisIconsData(0xf108, 'arrow_out');
+  static const arrow_right = _AegisIconsData(0xf109, 'arrow_right');
+  static const arrow_up = _AegisIconsData(0xf10a, 'arrow_up');
+  static const bookmark_fill = _AegisIconsData(0xf10b, 'bookmark_fill');
+  static const bookmark = _AegisIconsData(0xf10c, 'bookmark');
+  static const camera_fill = _AegisIconsData(0xf10d, 'camera_fill');
+  static const camera = _AegisIconsData(0xf10e, 'camera');
+  static const chat = _AegisIconsData(0xf10f, 'chat');
+  static const check_circle_fill = _AegisIconsData(0xf110, 'check_circle_fill');
+  static const check_circle = _AegisIconsData(0xf111, 'check_circle');
+  static const check = _AegisIconsData(0xf112, 'check');
+  static const chevron_down = _AegisIconsData(0xf113, 'chevron_down');
+  static const chevron_left = _AegisIconsData(0xf114, 'chevron_left');
+  static const chevron_right = _AegisIconsData(0xf115, 'chevron_right');
+  static const chevron_up = _AegisIconsData(0xf116, 'chevron_up');
+  static const close_circle_fill = _AegisIconsData(0xf117, 'close_circle_fill');
+  static const close_circle = _AegisIconsData(0xf118, 'close_circle');
+  static const close = _AegisIconsData(0xf119, 'close');
+  static const compare = _AegisIconsData(0xf11a, 'compare');
+  static const contact = _AegisIconsData(0xf11b, 'contact');
+  static const copy = _AegisIconsData(0xf11c, 'copy');
+  static const delete = _AegisIconsData(0xf11d, 'delete');
+  static const download_fill = _AegisIconsData(0xf11e, 'download_fill');
+  static const download = _AegisIconsData(0xf11f, 'download');
+  static const edit_fill = _AegisIconsData(0xf120, 'edit_fill');
+  static const edit = _AegisIconsData(0xf121, 'edit');
+  static const equal = _AegisIconsData(0xf122, 'equal');
+  static const eye_close = _AegisIconsData(0xf123, 'eye_close');
+  static const eye_open = _AegisIconsData(0xf124, 'eye_open');
+  static const face_id = _AegisIconsData(0xf125, 'face_id');
+  static const file_fill = _AegisIconsData(0xf126, 'file_fill');
+  static const file = _AegisIconsData(0xf127, 'file');
+  static const filter = _AegisIconsData(0xf128, 'filter');
+  static const fingerprint = _AegisIconsData(0xf129, 'fingerprint');
+  static const flash_fill = _AegisIconsData(0xf12a, 'flash_fill');
+  static const graph = _AegisIconsData(0xf12b, 'graph');
+  static const grid_off = _AegisIconsData(0xf12c, 'grid_off');
+  static const grid_on = _AegisIconsData(0xf12d, 'grid_on');
+  static const heart_fill = _AegisIconsData(0xf12e, 'heart_fill');
+  static const heart = _AegisIconsData(0xf12f, 'heart');
+  static const home_2 = _AegisIconsData(0xf130, 'home_2');
+  static const home_fill = _AegisIconsData(0xf131, 'home_fill');
+  static const home = _AegisIconsData(0xf132, 'home');
+  static const information_fill = _AegisIconsData(0xf133, 'information_fill');
+  static const information = _AegisIconsData(0xf134, 'information');
+  static const lock_fill = _AegisIconsData(0xf135, 'lock_fill');
+  static const menu = _AegisIconsData(0xf136, 'menu');
+  static const money = _AegisIconsData(0xf137, 'money');
+  static const more_horizontal = _AegisIconsData(0xf138, 'more_horizontal');
+  static const more_vertical = _AegisIconsData(0xf139, 'more_vertical');
+  static const netral = _AegisIconsData(0xf13a, 'netral');
+  static const order_history = _AegisIconsData(0xf13b, 'order_history');
+  static const placeholder = _AegisIconsData(0xf13c, 'placeholder');
+  static const play = _AegisIconsData(0xf13d, 'play');
+  static const portfolio = _AegisIconsData(0xf13e, 'portfolio');
+  static const product = _AegisIconsData(0xf13f, 'product');
+  static const profil = _AegisIconsData(0xf140, 'profil');
+  static const promo_code = _AegisIconsData(0xf141, 'promo_code');
+  static const question = _AegisIconsData(0xf142, 'question');
+  static const refresh = _AegisIconsData(0xf143, 'refresh');
+  static const restart = _AegisIconsData(0xf144, 'restart');
+  static const safety = _AegisIconsData(0xf145, 'safety');
+  static const scale = _AegisIconsData(0xf146, 'scale');
+  static const scan = _AegisIconsData(0xf147, 'scan');
+  static const search_fill = _AegisIconsData(0xf148, 'search_fill');
+  static const search = _AegisIconsData(0xf149, 'search');
+  static const send_plane_fill = _AegisIconsData(0xf14a, 'send_plane_fill');
+  static const send_plane = _AegisIconsData(0xf14b, 'send_plane');
+  static const share = _AegisIconsData(0xf14c, 'share');
+  static const shield_check_fill = _AegisIconsData(0xf14d, 'shield_check_fill');
+  static const shield_keyhole_fill = _AegisIconsData(0xf14e, 'shield_keyhole_fill');
+  static const sort_filter = _AegisIconsData(0xf14f, 'sort_filter');
+  static const sort = _AegisIconsData(0xf150, 'sort');
+  static const star_fill = _AegisIconsData(0xf151, 'star_fill');
+  static const star = _AegisIconsData(0xf152, 'star');
+  static const target = _AegisIconsData(0xf153, 'target');
+  static const time = _AegisIconsData(0xf154, 'time');
+  static const transactions = _AegisIconsData(0xf155, 'transactions');
+  static const user_fill = _AegisIconsData(0xf156, 'user_fill');
+  static const user = _AegisIconsData(0xf157, 'user');
+  static const verified = _AegisIconsData(0xf158, 'verified');
 
-  static const IconData add_image =
-      IconData(0xe900, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData add =
-      IconData(0xe901, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData alert_fill =
-      IconData(0xe902, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData alert =
-      IconData(0xe903, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_down =
-      IconData(0xe904, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_in =
-      IconData(0xe905, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_left =
-      IconData(0xe906, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_out =
-      IconData(0xe907, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_right =
-      IconData(0xe908, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData arrow_up =
-      IconData(0xe909, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData bookmark_fill =
-      IconData(0xe90a, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData bookmark =
-      IconData(0xe90b, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData camera_fill =
-      IconData(0xe90c, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData camera =
-      IconData(0xe90d, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData chat =
-      IconData(0xe90e, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData check =
-      IconData(0xe90f, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData chevron_down =
-      IconData(0xe910, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData chevron_left =
-      IconData(0xe911, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData chevron_right =
-      IconData(0xe912, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData chevron_up =
-      IconData(0xe913, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData check_circle_fill =
-      IconData(0xe914, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData check_circle =
-      IconData(0xe915, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData close_circle_fill =
-      IconData(0xe916, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData close_circle =
-      IconData(0xe917, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData close =
-      IconData(0xe918, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData compare =
-      IconData(0xe919, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData contact =
-      IconData(0xe91a, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData copy =
-      IconData(0xe91b, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData delete =
-      IconData(0xe91c, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData download_fill =
-      IconData(0xe91d, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData download =
-      IconData(0xe91e, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData edit_fill =
-      IconData(0xe91f, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData edit =
-      IconData(0xe920, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData equal =
-      IconData(0xe921, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData eye_close =
-      IconData(0xe922, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData eye_open =
-      IconData(0xe923, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData face_id =
-      IconData(0xe924, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData file_fill =
-      IconData(0xe925, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData file =
-      IconData(0xe926, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData filter =
-      IconData(0xe927, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData fingerprint =
-      IconData(0xe928, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData flash_fill =
-      IconData(0xe929, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData graph =
-      IconData(0xe92a, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData heart_fill =
-      IconData(0xe92b, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData heart =
-      IconData(0xe92c, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData home_fill =
-      IconData(0xe92d, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData home =
-      IconData(0xe92e, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData home_alt =
-      IconData(0xe92f, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData information_fill =
-      IconData(0xe930, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData information =
-      IconData(0xe931, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData lock_fill =
-      IconData(0xe932, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData menu =
-      IconData(0xe933, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData money =
-      IconData(0xe934, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData more_horizontal =
-      IconData(0xe935, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData more_vertical =
-      IconData(0xe936, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData netral =
-      IconData(0xe937, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData order_history =
-      IconData(0xe938, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData placeholder =
-      IconData(0xe939, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData play =
-      IconData(0xe93a, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData portfolio =
-      IconData(0xe93b, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData product =
-      IconData(0xe93c, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData profil =
-      IconData(0xe93d, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData promocode =
-      IconData(0xe93e, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData question =
-      IconData(0xe93f, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData refresh =
-      IconData(0xe940, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData restart =
-      IconData(0xe941, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData safety =
-      IconData(0xe942, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData scale =
-      IconData(0xe943, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData scan =
-      IconData(0xe944, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData search_fill =
-      IconData(0xe945, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData search =
-      IconData(0xe946, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData send_plane_fill =
-      IconData(0xe947, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData send_plane =
-      IconData(0xe948, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData share =
-      IconData(0xe949, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData shield_check_fill =
-      IconData(0xe94a, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData shield_keyhole_fill =
-      IconData(0xe94b, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData sort =
-      IconData(0xe94c, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData sort_filter =
-      IconData(0xe94d, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData star_fill =
-      IconData(0xe94e, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData star =
-      IconData(0xe94f, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData target =
-      IconData(0xe950, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData time =
-      IconData(0xe951, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData transactions =
-      IconData(0xe952, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData user_fill =
-      IconData(0xe953, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData user =
-      IconData(0xe954, fontFamily: _fontFamily, fontPackage: 'aegis');
-  static const IconData verified =
-      IconData(0xe955, fontFamily: _fontFamily, fontPackage: 'aegis');
+  static const all = <String, _AegisIconsData>{
+    'add_image': add_image,
+    'add': add,
+    'alert_fill': alert_fill,
+    'alert': alert,
+    'arrow_down': arrow_down,
+    'arrow_in': arrow_in,
+    'arrow_left': arrow_left,
+    'arrow_out': arrow_out,
+    'arrow_right': arrow_right,
+    'arrow_up': arrow_up,
+    'bookmark_fill': bookmark_fill,
+    'bookmark': bookmark,
+    'camera_fill': camera_fill,
+    'camera': camera,
+    'chat': chat,
+    'check_circle_fill': check_circle_fill,
+    'check_circle': check_circle,
+    'check': check,
+    'chevron_down': chevron_down,
+    'chevron_left': chevron_left,
+    'chevron_right': chevron_right,
+    'chevron_up': chevron_up,
+    'close_circle_fill': close_circle_fill,
+    'close_circle': close_circle,
+    'close': close,
+    'compare': compare,
+    'contact': contact,
+    'copy': copy,
+    'delete': delete,
+    'download_fill': download_fill,
+    'download': download,
+    'edit_fill': edit_fill,
+    'edit': edit,
+    'equal': equal,
+    'eye_close': eye_close,
+    'eye_open': eye_open,
+    'face_id': face_id,
+    'file_fill': file_fill,
+    'file': file,
+    'filter': filter,
+    'fingerprint': fingerprint,
+    'flash_fill': flash_fill,
+    'graph': graph,
+    'grid_off': grid_off,
+    'grid_on': grid_on,
+    'heart_fill': heart_fill,
+    'heart': heart,
+    'home_2': home_2,
+    'home_fill': home_fill,
+    'home': home,
+    'information_fill': information_fill,
+    'information': information,
+    'lock_fill': lock_fill,
+    'menu': menu,
+    'money': money,
+    'more_horizontal': more_horizontal,
+    'more_vertical': more_vertical,
+    'netral': netral,
+    'order_history': order_history,
+    'placeholder': placeholder,
+    'play': play,
+    'portfolio': portfolio,
+    'product': product,
+    'profil': profil,
+    'promo_code': promo_code,
+    'question': question,
+    'refresh': refresh,
+    'restart': restart,
+    'safety': safety,
+    'scale': scale,
+    'scan': scan,
+    'search_fill': search_fill,
+    'search': search,
+    'send_plane_fill': send_plane_fill,
+    'send_plane': send_plane,
+    'share': share,
+    'shield_check_fill': shield_check_fill,
+    'shield_keyhole_fill': shield_keyhole_fill,
+    'sort_filter': sort_filter,
+    'sort': sort,
+    'star_fill': star_fill,
+    'star': star,
+    'target': target,
+    'time': time,
+    'transactions': transactions,
+    'user_fill': user_fill,
+    'user': user,
+    'verified': verified,
+  };
 }
