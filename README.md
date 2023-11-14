840556f3a112c2c686373fad689ed45c880b490c
Yosyafat Aulia Ferdiansyah <112699456+yos-syky@users.noreply.github.com>
2023-11-14 15:24:18 +0700

    Feature: New image/illustrations asset (#4)
    * add: new image/illustrations asset
    
    * add: readme

--- README.md
+++ README.md
@@ -1,39 +1,37 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# _Aegis_
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+_Aegis_ is a library packages for _Athena_'s project that consist of UI components, formatters, icons, assets and others.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+- **UI Components**
+- **Formatters**
+- **Icons**
+- **Assets**
+- TBA
 
-## Getting started
+### UI Components
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+### Formatters
 
-## Usage
+### Icons
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
+#### Usage
 
 ```dart
-const like = 'sample';
+Icon(
+  AegisIcons.arrow_up,
+  color: AegisColors.neutral0,
+),
 ```
 
-## Additional information
+### Assets
 
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+#### Usage
+Always add line `package: 'aegis'` 
+```dart
+Image.asset(
+  AegisAssets.phone,
+  package: 'aegis',
+),
+```
--- lib/src/assets/assets.dart
+++ lib/src/assets/assets.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
 class AegisAssets {
   // Base
-  static const base = 'assets/illustrations';
+  static const base = 'lib/assets/illustrations';
 
   // Media
   static const email = '$base/media/email.png';
@@ -26,4 +28,50 @@ class AegisAssets {
   static const hourGlass = '$base/transaction/hourglass.png';
   static const switching = '$base/transaction/switching.png';
   static const redemption = '$base/transaction/redemption.png';
+
+  // Other
+  static const account_2 = '$base/account_2.png';
+  static const account = '$base/account.png';
+  static const cut_off_end_time = '$base/cut_off_end_time.png';
+  static const cut_off_start_time = '$base/cut_off_start_time.png';
+  static const dictionary_blue = '$base/dictionary_blue.png';
+  static const dictionary_red = '$base/dictionary_red.png';
+  static const empty_general_ghost = '$base/empty_general_ghost.png';
+  static const empty_general_leaves = '$base/empty_general_leaves.png';
+  static const go_to_pdp_1 = '$base/go_to_pdp_1.png';
+  static const go_to_pdp_2 = '$base/go_to_pdp_2.png';
+  static const how_to_buy = '$base/how_to_buy.png';
+  static const how_to_create_porto = '$base/how_to_create_porto.png';
+  static const how_to_sell = '$base/how_to_sell.png';
+  static const how_to_switch_2 = '$base/how_to_switch_2.png';
+  static const how_to_switch = '$base/how_to_switch.png';
+  static const index_benchmark = '$base/index_benchmark.png';
+  // static const lock = '$base/lock.png';
+  static const move_porto = '$base/move_porto.png';
+  static const no_nabung_rutin = '$base/no_nabung_rutin.png';
+  static const no_transaction_1 = '$base/no_transaction_1.png';
+  static const no_transaction_2 = '$base/no_transaction_2.png';
+  static const no_watchlist = '$base/no_watchlist.png';
+  static const not_found = '$base/not_found.png';
+  static const notification = '$base/notification.png';
+  // static const ojk = '$base/ojk.png';
+  static const redemption_time_1 = '$base/redemption_time_1.png';
+  static const redemption_time = '$base/redemption_time.png';
+  // static const redemption = '$base/redemption.png';
+  static const referral_code = '$base/referral_code.png';
+  static const referral = '$base/referral.png';
+  static const registration = '$base/registration.png';
+  static const reksa_dana_intro = '$base/reksa_dana_intro.png';
+  static const return_history = '$base/return_history.png';
+  static const sayakaya_recommendation = '$base/sayakaya_recommendation.png';
+  static const sayakaya = '$base/sayakaya.png';
+  static const select_manually_1 = '$base/select_manually_1.png';
+  static const select_manually_2 = '$base/select_manually_2.png';
+  static const statistic = '$base/statistic.png';
+  static const subscription = '$base/subscription.png';
+  // static const sucor = '$base/sucor.png';
+  static const switch_porto = '$base/switch_porto.png';
+  static const transaction_time = '$base/transaction_time.png';
+  // static const verified = '$base/verified.png';
+
 }
