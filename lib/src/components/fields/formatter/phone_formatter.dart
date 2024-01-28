7f7fa01f34d4679c6d6e13eb913e0067faa6afe1
Dany Rizkiawan <dany@sayakaya.id>
2024-01-28 10:44:45 +0700

    fix: phone formatter

--- lib/src/components/fields/formatter/phone_formatter.dart
+++ lib/src/components/fields/formatter/phone_formatter.dart
@@ -9,26 +9,23 @@ class PhoneFormatter extends TextInputFormatter {
     TextEditingValue newValue,
   ) {
     var newText = newValue.text;
-    if (newText.isNotEmpty) {
-      var oldText = oldValue.text;
-      if (newText.length > oldText.length) {
-        if (SkPhoneValidator.showPhoneCode(newText) &&
-            SkPhoneValidator.showPhoneCode(oldText)) {
-          if (newText.startsWith('+62') && newText.length >= 9) {
-            newText = newText.replaceFirst('+62', '');
-          } else if (newText.startsWith('62') && newText.length >= 8) {
-            newText = newText.replaceFirst('62', '');
-          } else if (newText.startsWith('0')) {
-            newText = newText.replaceFirst('0', '');
-          }
-          return TextEditingValue(
-            text: newText,
-            selection: TextSelection.collapsed(offset: newText.length),
-          );
-        }
-      }
+    if (newText.isEmpty) return newValue;
+
+    if (!SkPhoneValidator.showPhoneCode(newText)) return newValue;
+
+    if (newText.startsWith('+62')) {
+      newText = newText.replaceFirst('+62', '');
+    } else if (newText.startsWith('62')) {
+      newText = newText.replaceFirst('62', '');
+    } else if (newText.startsWith('0')) {
+      newText = newText.replaceFirst('0', '');
     }
 
-    return newValue;
+    if (!SkPhoneValidator.showPhoneCode(newText)) return newValue;
+
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length),
+    );
   }
 }
