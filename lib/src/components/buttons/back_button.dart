d20f14d6af9d1704c1caa8ab9493b1d2fb61ad54
Yosyafat Aulia <yosyafat@sayakaya.id>
2024-11-06 11:47:38 +0700

    fix: back button semantics

--- lib/src/components/buttons/back_button.dart
+++ lib/src/components/buttons/back_button.dart
@@ -1,3 +1,4 @@
+import 'package:aegis/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,14 +13,17 @@ class SkBackButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      padding: EdgeInsets.zero,
-      onPressed: onPressed,
-      icon: Icon(
-        AegisIcons.chevron_left,
-        size: 22.r,
-        color: color ?? AegisColors.iconHighEmphasis,
-        semanticLabel: 'back_button',
+    return SkSemantics(
+      identifier: 'back_button',
+      excludeSemantics: true,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: onPressed,
+        icon: Icon(
+          AegisIcons.chevron_left,
+          size: 22.r,
+          color: color ?? AegisColors.iconHighEmphasis,
+        ),
       ),
     );
   }
