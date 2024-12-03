25b30da428f041265a223cabd2eaea69cea1595f
Dany Rizkiawan <dany@sayakaya.id>
2024-12-03 21:26:51 +0700

    add: color

---
+++ lib/src/styles/color/color.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class AegisColors {
+  AegisColors._();
+
+  static const transparent = Color.fromRGBO(0, 0, 0, 0);
+
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+
+  static const neutral100 = Color.fromRGBO(245, 246, 250, 1);
+  static const neutral200 = Color.fromRGBO(224, 227, 239, 1);
+  static const neutral300 = Color.fromRGBO(152, 156, 172, 1);
+  static const neutral400 = Color.fromRGBO(86, 91, 107, 1);
+  static const neutral500 = Color.fromRGBO(28, 29, 31, 1);
+  static const neutral600 = Color.fromRGBO(0, 0, 0, 1);
+
+  static const neutralVariant300 = Color.fromRGBO(255, 255, 255, 0.2);
+  static const neutralVariant400 = Color.fromRGBO(71, 74, 90, 1);
+  static const neutralVariant500 = Color.fromRGBO(45, 45, 45, 0.32);
+  static const neutralVariant600 = Color.fromRGBO(4, 8, 22, 0.6);
+
+  static const purple100 = Color.fromRGBO(236, 234, 247, 1);
+  static const purple200 = Color.fromRGBO(160, 149, 216, 1);
+  static const purple300 = Color.fromRGBO(103, 86, 193, 1);
+  static const purple400 = Color.fromRGBO(83, 71, 148, 1);
+
+  static const purpleVariant100 = Color.fromRGBO(136, 153, 231, 1);
+  static const purpleVariant200 = Color.fromRGBO(98, 121, 224, 1);
+
+  static const blue100 = Color.fromRGBO(238, 240, 252, 1);
+  static const blue200 = Color.fromRGBO(136, 153, 231, 1);
+  static const blue300 = Color.fromRGBO(77, 104, 220, 1);
+  static const blue400 = Color.fromRGBO(58, 80, 171, 1);
+
+  static const tosca100 = Color.fromRGBO(230, 254, 252, 1);
+  static const tosca200 = Color.fromRGBO(112, 218, 211, 1);
+  static const tosca300 = Color.fromRGBO(20, 198, 198, 1);
+  static const tosca400 = Color.fromRGBO(11, 161, 161, 1);
+
+  static const green100 = Color.fromRGBO(238, 255, 249, 1);
+  static const green200 = Color.fromRGBO(112, 218, 181, 1);
+  static const green300 = Color.fromRGBO(20, 198, 135, 1);
+  static const green400 = Color.fromRGBO(19, 175, 120, 1);
+
+  static const yellow100 = Color.fromRGBO(253, 255, 229, 1);
+  static const yellow200 = Color.fromRGBO(255, 238, 148, 1);
+  static const yellow300 = Color.fromRGBO(255, 222, 49, 1);
+
+  static const orange100 = Color.fromRGBO(255, 235, 229, 1);
+  static const orange200 = Color.fromRGBO(255, 157, 126, 1);
+  static const orange300 = Color.fromRGBO(255, 98, 49, 1);
+
+  static const red100 = Color.fromRGBO(255, 229, 236, 1);
+  static const red200 = Color.fromRGBO(255, 126, 159, 1);
+  static const red300 = Color.fromRGBO(255, 49, 101, 1);
+
+  static const backgroundWhite = white;
+  static const backgroundGray = neutral100;
+  static const backgroundSurface = Color.fromRGBO(27, 27, 27, 1);
+  static const backgroundBlack = Color.fromRGBO(13, 13, 13, 1);
+
+  static const textHighEmphasis = neutral500;
+  static const textLowEmphasis = neutral400;
+  static const textDisabled = neutral300;
+  static const textPositive = green400;
+
+  static const iconHighEmphasis = neutral500;
+  static const iconLowEmphasis = neutral400;
+  static const iconDisabled = neutral200;
+
+  static const borderHighEmphasis = neutral200;
+  static const borderLowEmphasis = neutral100;
+  static const borderNegative = red300;
+
+  static const shadow = Color.fromRGBO(0, 0, 0, 0.08);
+  static const shadowEvelation1 = Color.fromRGBO(48, 49, 53, 0.16);
+}
---
+++ lib/styles.dart
@@ -0,0 +1,3 @@
+library styles;
+
+export 'src/styles/color/color.dart';
