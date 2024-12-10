abdaa70af32570f5614128913c5177424c5ffb9e
Dany Rizkiawan <dany@sayakaya.id>
2024-12-10 14:06:06 +0700

    add: scaffold component

--- lib/src/components/scaffolds/scaffold.dart
+++ lib/src/components/scaffolds/scaffold.dart
@@ -1,39 +1,32 @@
 import 'package:flutter/material.dart';
 
-import '../../../foundation.dart';
+import '../../../components.dart';
 
 class SkScaffold extends StatelessWidget {
-  final PreferredSizeWidget? appBar;
-  final Widget? body;
-  final Color backgroundColor;
+  /// The primary content of the scaffold.
+  final Widget body;
+
+  /// Please use [SkAppBar] to maintain a consistent app bar
+  final SkAppBar? appBar;
+
+  /// This can be used for bottom actions (e.g., BottomNavigationBar, Button)
   final Widget? bottomNavigationBar;
-  final List<Widget>? persistentFooterButtons;
-  final bool? resizeToAvoidBottomInset;
 
   const SkScaffold({
     Key? key,
+    required this.body,
     this.appBar,
-    this.body,
-    this.backgroundColor = AegisColors.neutral0,
     this.bottomNavigationBar,
-    this.persistentFooterButtons,
-    this.resizeToAvoidBottomInset,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-      child: Theme(
-        data: ThemeData(),
-        child: Scaffold(
-          backgroundColor: backgroundColor,
-          appBar: appBar,
-          body: body,
-          persistentFooterButtons: persistentFooterButtons,
-          bottomNavigationBar: bottomNavigationBar,
-          resizeToAvoidBottomInset: resizeToAvoidBottomInset,
-        ),
+      child: Scaffold(
+        appBar: appBar,
+        body: body,
+        bottomNavigationBar: bottomNavigationBar,
       ),
     );
   }
