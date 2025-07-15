a530e1076cca9f217260d44fb71f41f23ac8193a
Dany Rizkiawan <dany@sayakaya.id>
2025-07-15 13:48:22 +0700

    fix: widget util

--- lib/src/utils/widget_util.dart
+++ lib/src/utils/widget_util.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 extension WidgetUtil on BuildContext {
   double get bottomPadding {
     var bottom = MediaQuery.paddingOf(this).bottom;
-    if (bottom == 0) bottom += 16;
+    if (Platform.isAndroid || (Platform.isIOS && bottom == 0)) bottom += 16;
     return bottom;
   }
 }
