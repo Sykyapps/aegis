ac16a6b8b021e24397b58bbd54d60ec71fd6eb2e
Dany Rizkiawan <dany@sayakaya.id>
2025-07-15 13:33:22 +0700

    feat: add widget util

---
+++ lib/src/utils/widget_util.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension WidgetUtil on BuildContext {
+  double get bottomPadding {
+    var bottom = MediaQuery.paddingOf(this).bottom;
+    if (bottom == 0) bottom += 16;
+    return bottom;
+  }
+}
--- lib/utils.dart
+++ lib/utils.dart
@@ -2,3 +2,4 @@ library utils;
 
 export 'src/utils/currency_util.dart';
 export 'src/utils/measurement_util.dart';
+export 'src/utils/widget_util.dart';
