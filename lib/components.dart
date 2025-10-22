dd8cbf99788310d0eb1b2e1a7e2220f6bfec2f19
Dany Rizkiawan <dany@sayakaya.id>
2025-10-23 06:46:02 +0700

    feat: add info chip

--- lib/components.dart
+++ lib/components.dart
@@ -4,6 +4,7 @@ export 'src/components/animations/animations.dart';
 export 'src/components/app_bars/app_bars.dart';
 export 'src/components/bottom_sheet/bottom_sheets.dart';
 export 'src/components/checkboxes/checkboxes.dart';
+export 'src/components/chips/chips.dart';
 export 'src/components/buttons/buttons.dart';
 export 'src/components/card/card.dart';
 export 'src/components/fields/fields.dart';
