9b0e5c032b2e310fcabda9783e1cbf79b3cd15a1
Dany Rizkiawan <dany@sayakaya.id>
2026-07-28 12:19:39 +0700

    fix: bottom sheet header

--- lib/src/components/bottom_sheet/bottom_sheet.dart
+++ lib/src/components/bottom_sheet/bottom_sheet.dart
@@ -11,6 +11,7 @@ class SkBottomSheet extends HookWidget {
     Key? key,
     required this.child,
     this.title,
+    this.expandedHeight = 100,
     this.backgroundImage,
     this.barrierColor,
     this.showHandle = true,
@@ -18,6 +19,7 @@ class SkBottomSheet extends HookWidget {
   }) : super(key: key);
 
   final String? title;
+  final double expandedHeight;
   final ImageProvider<Object>? backgroundImage;
   final Widget? child;
   final Color? barrierColor;
@@ -78,11 +80,10 @@ class SkBottomSheet extends HookWidget {
               shrinkWrap: true,
               physics: const ClampingScrollPhysics(),
               slivers: [
-                _Header(
-                  title: title,
-                  backgroundColor: isPinned.value
-                      ? AegisColors.backgroundWhite
-                      : AegisColors.transparent,
+                SkSliverAppBar(
+                  title: title ?? '',
+                  expandedHeight: expandedHeight,
+                  leading: const _CloseButton(),
                 ),
                 SliverToBoxAdapter(child: child),
               ],
@@ -94,121 +95,26 @@ class SkBottomSheet extends HookWidget {
   }
 }
 
-class _Header extends StatelessWidget {
-  const _Header({
-    Key? key,
-    this.title,
-    this.backgroundColor,
-  }) : super(key: key);
-
-  final String? title;
-  final Color? backgroundColor;
-
-  static final double height = 56.r;
-  static final double expandedHeight = 80.r;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      pinned: true,
-      floating: true,
-      scrolledUnderElevation: 0,
-      automaticallyImplyLeading: false,
-      toolbarHeight: backgroundColor == AegisColors.transparent ? 64.r : height,
-      expandedHeight: title == null ? null : expandedHeight,
-      backgroundColor: backgroundColor ?? AegisColors.neutral0,
-      centerTitle: false,
-      titleSpacing: 0,
-      title: const _CloseButton(),
-      flexibleSpace: title == null
-          ? null
-          : LayoutBuilder(
-              builder: (context, constraint) {
-                var top = constraint.biggest.height;
-                return FlexibleSpaceBar(
-                  expandedTitleScale: 1,
-                  titlePadding: EdgeInsets.zero,
-                  title: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 20.r,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              // const _CloseButton(),
-                              SizedBox(width: 36.r),
-                              Expanded(
-                                child: Visibility(
-                                  visible: top <= 75,
-                                  maintainAnimation: true,
-                                  maintainState: true,
-                                  child: AnimatedOpacity(
-                                    opacity: top <= 75 ? 1 : 0,
-                                    duration: const Duration(milliseconds: 300),
-                                    child: Text(
-                                      title!,
-                                      textAlign: TextAlign.left,
-                                      style: AegisFont.headlineSmall.copyWith(
-                                        color: AegisColors.neutral500,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Visibility(
-                        visible: top >= 75,
-                        maintainAnimation: true,
-                        maintainState: true,
-                        child: AnimatedOpacity(
-                          opacity: top >= 75 ? 1 : 0,
-                          duration: const Duration(milliseconds: 300),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20).r,
-                            child: Text(
-                              title!,
-                              style: AegisFont.headlineMedium.copyWith(
-                                color: AegisColors.neutral500,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-    );
-  }
-}
-
 class _CloseButton extends StatelessWidget {
   const _CloseButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => Navigator.of(context).pop(),
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(8.r),
-        shape: const CircleBorder(),
-        backgroundColor: AegisColors.backgroundWhite,
-        foregroundColor: AegisColors.textHighEmphasis,
-        elevation: 0,
-      ),
-      child: Icon(
-        AegisIcons.close,
-        size: 20.sp,
+    return GestureDetector(
+      onTap: () => Navigator.of(context).pop(),
+      child: Container(
+        width: 40,
+        height: 40,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          color: AegisColors.backgroundWhite,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          AegisIcons.close,
+          size: 20.sp,
+          color: AegisColors.textHighEmphasis,
+        ),
       ),
     );
   }
